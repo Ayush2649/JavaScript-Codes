7bd06fff11a9bffdc066ac1f1c9489469bc21151
Ayush Sahu <ayushsahu1430@gmail.com>
2024-09-22 10:16:48 +0530

    Learnt various methods of string

---
+++ js2.js
@@ -0,0 +1,53 @@
+// let msg = "      Hello       ";
+// console.log(msg);
+// console.log(msg.trim());
+// Rmeoves the white spaces int front and end of the string and not in between
+// It does not changes the real string it creates a new instance of the string and displays the instance
+// This property of the string is known as immutable
+// Every string in JavaScript is immutable
+
+// let myname = "Ayush Sahu";
+// console.log(myname);
+// console.log(myname.toUpperCase());
+// console.log(myname.toLowerCase());
+
+// let msg = "ILoveCoding";
+// console.log(msg.indexOf("Love"));
+// console.log(msg.indexOf("j"));
+// console.log(msg.indexOf("o"));
+
+// Method Chaining
+// let msg = "      Hello       ";
+// console.log("Original String : " + msg);
+// let newMsg = msg.trim();
+// console.log("String after trim : " + newMsg);
+// newMsg = newMsg.toUpperCase();
+// console.log("String after toUpperCaase : " + newMsg);
+// This is the normal way to apply different methods to a string
+// let newmsg = msg.trim().toUpperCase(); // This is Method Chaining
+// console.log(newmsg);
+
+// let msg = "Hello";
+// let newMsg = msg.slice(0, 3);
+// let newmsg = msg.slice(-2); // It slices the length of the string minus that number
+// console.log(newmsg);
+
+// Replace Method
+// let msg = "ILoveCoding";
+// console.log(msg);
+// console.log(msg.replace("Love", "do"));
+
+// Repeat Method
+// let fruit = "mango";
+// console.log(fruit.repeat(2));
+
+// let msg = "help!";
+// console.log(msg.trim().toUpperCase());
+
+// let ame = "ApnaCollege";
+// console.log(ame.slice(4, 9));
+// console.log(ame.indexOf("na"));
+// console.log(ame.replace("Apna", "Our"));
+
+// let sname = ame.slice(4);
+// console.log(sname.replace("l", "t"));
