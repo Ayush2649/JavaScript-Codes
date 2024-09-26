e9c9f9c35ab50a576623e415a0d824a129ab841d
Ayush Sahu <ayushsahu1430@gmail.com>
2024-09-26 17:19:46 +0530

    Learnt some concepts of loops

---
+++ loops.js
@@ -0,0 +1,36 @@
+// for(let i = 1; i <= 5; i++){
+//     console.log(i);
+// } // 1, 2, 3, 4, 5
+
+// for(let i = 1; i <= 15; i++){
+//     if((i % 2) != 0){
+//         console.log(i);
+//     }
+// } // 1, 3, 5, 7, 9, 11, 13, 15
+
+// for(let i = 0; i < 15; i++){
+//     if((i % 2) == 0){
+//         console.log(i);
+//     }
+// } // 0, 2, 4, 6, 8, 10, 12, 14
+
+// for(let i = 0; i < 15; i = i + 2){
+//     console.log(i);
+// } // 0, 2, 4, 6, 8, 10, 12, 14
+
+// for(let i = 15; i >= 0; i = i - 2){
+//     console.log(i);
+// }
+
+// let n = prompt("Write your Number : ");
+// n = parseInt(n); // Returns the numerical value
+// for(let i = n; i <= n * 10; i = i + n){
+//     console.log(i);
+// } Multiplication Table
+
+for(let i = 1; i <= 3; i++){
+    console.log(`Outer loop ${i}`);
+    for(let j = 1; j <= 3; j++){
+        console.log(j);
+    }
+} // Nested Lopps
