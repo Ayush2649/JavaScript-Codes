803e6e91d955f68778e5ec63e14ed5c4575a9ec6
Ayush Sahu <ayushsahu1430@gmail.com>
2024-09-27 09:51:04 +0530

    Completed the Objects part

---
+++ objects.js
@@ -0,0 +1,115 @@
+// console.log("hello World");
+
+// const student = {
+//     name : "Ayush",
+//     marks : 87,
+//     age : 20
+// };
+
+// console.log(student); // {name: 'Ayush', marks: 87, age: 20}
+
+// const post = {
+//     username : "@ayush_4911",
+//     content : "Hey, Hii I am Ayush",
+//     likes : 240,
+//     retweets : 40,
+//     tags : 10
+// };
+
+// console.log(post); // {username: '@ayush_4911', content: 'Hey, Hii I am Ayush', likes: 240, retweets: 40, tags: 10}
+// console.log(post["content"]); // Hey, Hii I am Ayush
+// console.log(post.likes); // 240 
+// Keys are automatically converted into strings no matter what their names are
+
+// const student = {
+//     name: "Ayush",
+//     marks: 90,
+//     age: 20,
+//     city: "Mumbai"
+// };
+
+// console.log(student); // {name: 'Ayush', marks: 90, age: 20, city: 'Mumbai'}
+// console.log(student.city = "Brahmapur"); // Update any attribute
+// console.log(student); // {name: 'Ayush', marks: 90, age: 20, city: 'Brahmapur'}
+// console.log(student.gender = "Male"); // Add any new attribute
+// console.log(student); // {name: 'Ayush', marks: 90, age: 20, city: 'Brahmapur', gender: 'Male'}
+
+// const studentInfo = {
+//     Ayush : {
+//         age : 20,
+//         grade : "A+"
+//     },
+//     Vaibhav : {
+//         age : 20,
+//         grade : "O"
+//     },
+//     Prayas : {
+//         age : 20,
+//         grade : "A"
+//     },
+//     Swapnil : {
+//         age : 21,
+//         grade : "B"
+//     }
+// };
+
+// console.log(studentInfo); // Nested Objects
+
+// const studentInfo = [
+//     {
+//         name : "Ayush",
+//         age : 20,
+//         marks : 92
+//     },
+//     {
+//         name : "Vaibhav",
+//         age : 20,
+//         marks : 96
+//     },
+//     {
+//         name : "Swapnil",
+//         age : 21,
+//         marks : 70
+//     }
+// ];
+
+// console.log(studentInfo); // Array of Objects
+
+// console.log(Math.PI); // 3.141592653589793
+// console.log(Math.E); // 2.718281828459045
+// console.log(Math.abs(-79.65)); // 79.65 Gives the positive value always
+// console.log(Math.pow(2, 7)); // 128
+// console.log(Math.floor(79.65)); // 79 Gives the round off value - less than or equal to value
+// console.log(Math.ceil(79.65)); // 80 Gives the round off value - greater than or equal to value
+// console.log(Math.random()); // 0.2371349626133119 Returns any random value between 0 and 1 but not 1
+
+// let random = Math.floor(Math.random() * 10) + 1;
+// console.log(random);// Returns a value between 1 and 10
+
+// let random = Math.floor(Math.random() * 100) + 1;
+// console.log(random);
+
+// let random = Math.floor(Math.random() * 5) + 1;
+// console.log(random);
+
+// let random = Math.floor(Math.random() * 6) + 1;
+// console.log(random);
+
+// const car = {
+//     name : "Mercedes",
+//     model : "MayBach",
+//     color : "Brown"
+// };
+// console.log(car.name); Mercedes
+
+// const person = {
+//     name : "George",
+//     age : 22,
+//     city : "Chicago"
+// }; 
+// console.log(person); // {name: 'George', age: 22, city: 'Chicago'}
+// console.log(person.city = "New York");
+// console.log(person); // {name: 'George', age: 22, city: 'New York'}
+// console.log(person.country = "United States");
+// console.log(person); // {name: 'George', age: 22, city: 'New York', country: 'United States'}
+
