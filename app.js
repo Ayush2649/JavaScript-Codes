97dbd519db2bc6d61672a767e87ea6f07fac3eca
Ayush Sahu <ayushsahu1430@gmail.com>
2024-10-01 18:51:15 +0530

    Learning DOM Manipulation in JS

---
+++ app.js
@@ -0,0 +1,33 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0; i < smallImages.length; i++){
+//     smallImages[i].src = "spiderman_img.png";
+//     console.dir(`The source of the immage ${i} is changed`);
+// }
+
+// console.dir(document.getElementById("mainImg"));
+
+// console.dir(document.getElementsByTagName("p"));
+
+// console.dir(document.querySelector("p")); // Return the first occourence of p
+
+// console.dir(document.querySelector(".oldImg"));
+
+// console.dir(document.querySelector("#description"));
+
+// console.dir(document.querySelector("div a"));
+
+// console.dir(document.querySelectorAll("div a")); // Returns all the occourances of a in the div
+
+// let para = document.querySelector("p");
+
+// console.dir(para.innerText); // It shows the entire text that is visible in the para
+
+// console.dir(para.innerHTML); // It shows the entire markup of the para
+
+// console.dir(para.textContent); // It shows the text in the way it has been written in the html document
+
+// let img = document.querySelector("#mainImg");
+
+// console.dir(img.getAttribute("id"));
+
