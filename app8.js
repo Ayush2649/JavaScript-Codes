d17b4935b96474c9897a366cab5b833709c6b389
Ayush Sahu <ayushsahu1430@gmail.com>
2024-10-02 17:30:38 +0530

    Learnt various Event Handling functions in JS

--- app8.js
+++ app8.js
@@ -33,24 +33,74 @@
 //     alert("Ayush Sahu");
 // }
 
-let btn = document.querySelector("button");
+// let btn = document.querySelector("button");
 
-btn.addEventListener("click", function () {
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText = randomColor;
+// btn.addEventListener("click", function () {
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColor();
+//     h3.innerText = randomColor;
 
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
+//     let div = document.querySelector("div");
+//     div.style.backgroundColor = randomColor;
 
-    console.log("Colour was generated");
-});
+//     console.log("Colour was generated");
+// });
 
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
+// function getRandomColor() {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
 
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    return color;
-}
+//     let color = `rgb(${red}, ${green}, ${blue})`;
+//     return color;
+// }
+
+
+// let btn = document.querySelector("button");
+// let h1 = document.querySelector("h1");
+// let h3 = document.querySelector("h3");
+// let p = document.querySelector("p");
+
+// function changeColor() {
+//     console.dir(this.innerText);
+//     this.style.color = "blue";
+// };
+
+// btn.addEventListener("click", changeColor)
+
+// h1.addEventListener("click", changeColor);
+
+// h3.addEventListener("click", changeColor);
+
+// p.addEventListener("click", changeColor);
+
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+
+//     let user = document.querySelector("#user");
+//     let pass = document.querySelector("#pass");
+//     console.log(user.value);
+//     console.log(pass.value);
+
+//     alert(`Hii ${user.value}, your password is set to ${pass.value}`);
+// })
+
+// let inp = document.querySelector("input");
+// let h1 = document.querySelector("h1");
+
+// inp.addEventListener("change", function() {
+//     console.log(inp.value);
+//     h1.innerText = inp.value;
+// });
+
+let body = document.querySelector("body");
+let btn = document.createElement("button");
+body.append(btn);
+btn.innerText = "Click Me!!";
+let btn1 = document.querySelector("button");
+
+btn1.addEventListener("click", function() {
+    btn1.style.backgroundColor = "green";
+})
