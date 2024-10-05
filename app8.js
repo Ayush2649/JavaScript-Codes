e0bbb09b71818f9a9e1e959973b73ef443211911
Ayush Sahu <ayushsahu1430@gmail.com>
2024-10-05 18:55:58 +0530

    Learning Asynchronous nature of JS

--- app8.js
+++ app8.js
@@ -136,3 +136,18 @@
 //     ul.appendChild(item);
 //     inp.value = "";
 // })
+
+function one() {
+    return 1;
+}
+
+function two() {
+    return one() + one();
+}
+
+function three() {
+    let ans = two() + one();
+    console.log(ans);
+}
+
+three();
